922e85042b356dd7d0e7dfc2b174e9c59113659d
raymond390 <74500294+raymond390@users.noreply.github.com>
2022-01-13 17:03:25 +0100

    test

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
  "verification": "",
- "description": "Pogy is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "description": "test bot",
  "domain": "", // domain
  "google_analitics": "", // google analitics
  "token": process.env.TOKEN,
- "https":"https://", // leave as is
+ "https":"htt   ps://", // leave as is
  "port":"5003",
 
- "client_id":"", // bot client ID
- "secret":"" // bot client secret for auth
+ "client_id":"761893829988319292", // bot client ID
+ "secret":"tifU8mXFHYLVJWDLKBjyLuwkr07CA2ig" // bot client secret for auth
 
 }
 
